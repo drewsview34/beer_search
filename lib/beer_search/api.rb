761b1adac5d28558fa06c949dfc3daefb40c90ce
drewsview34 <drewsview34@outlook.com>
2020-08-09 19:32:53 -0700

    Submitting code for project review

--- lib/beer_search/api.rb
+++ lib/beer_search/api.rb
@@ -1,9 +1,8 @@
 class API
     def self.get_response
       response = HTTParty.get('https://api.punkapi.com/v2/beers')
-      response.map do |hash|
-        BeerSearchClass.new(hash['id'])
-        binding.pry
+      response.each do |hash|
+        BeerSearchClass.new(hash['id'],hash['name'],hash['tagline'],hash['description'],hash['abv'])
       end
     end
   end
--- lib/beer_search/cli.rb
+++ lib/beer_search/cli.rb
@@ -1,7 +1,48 @@
 class CLI
     
     def run
-        API.get_response
+        welcome
     end
-    
+
+    def welcome
+        puts "******* Welcome to Beer Search *******"
+        puts "******* Are you 21 Years of Age *******"
+        puts "******* Please enter y/n *******"
+        input = gets.chomp
+        if input == "y"
+            puts "Great! Lets Search for Some Beers"
+            search_menu
+        else
+            puts "Sorry! You're Not Old Enough."
+        end
+    end
+
+    def search_menu
+        # while input != "exit"
+        puts "******* Please Enter How You Would Like to Search... *******"
+        puts "******* type 1 for all beers / 2 for taglines / 3 for description/ 5 for abv OR Exit to End the Search *******"
+        input = gets.chomp
+        if input == "1"
+            display_beers
+        elsif input == "2"
+            API.get_response
+            BeerSearchClass.all.each.with_index(1) do |obj, index|
+                puts "#{index}: #{obj.tagline}"
+            end
+        elsif input = "3"
+            display_beers
+        else 
+           puts "exit"
+        end
+    end
+
+
+    def display_beers
+        API.get_response
+        BeerSearchClass.all.each.with_index(1) do |obj, index|
+            puts "#{index}: #{obj.name}"
+        end
+
+    end
+
 end
